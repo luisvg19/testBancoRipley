4b9bf9b999ab8c374a8c53c8fb12243451520369
luisvg19 <luisvg19@gmail.com>
2022-09-29 20:47:30 -0300

    add front

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+        res.send('Hola mundo v3');
+    });
+
+const port = 8080;
+const server = app.listen(port, () => {
+        console.log('listening on port %s.\n', server.address().port);
+    });
+
+module.exports = app;
